44cd98e0460a93209b240689b64fe781a82bfa32
marcello <marcello@marcello-pc.homenet.telecomitalia.it>
2019-12-14 20:09:19 +0100

    Aggiunta ricerca della città sulla mappa

--- Pagina/funzioni_pagina.js
+++ Pagina/funzioni_pagina.js
@@ -39,9 +39,28 @@ window.setTimeout("updateDate()",1000);
 }
 
 var map;
+var geocoder;
+
 function initMap() {
+    geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42, lng: 13},
     zoom: 5 
     });
 }
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+ 
+  }
