cdd9b1eb9f1aa1cd2663fc6c97ac50225bd2a073
LNPacio <lucapacilli44@gmail.com>
2019-12-20 12:21:21 +0100

    reg8

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/signup', function(req,res){
     var password = req.body.inputPassword;
     var surname = req.body.surname;
     
-	client.query("INSERT INTO utente VALUES("+email+", "+name+","+surname+", "+password+")", (err, res) => {
+	client.query('INSERT INTO utente(email, name, surname, password) VALUES($1,$2,$3,$4)', [email, name, surname, password], (err, res) => {
 		if (err){ throw err};
 		res.status(201).send('User added');
 		client.end();
