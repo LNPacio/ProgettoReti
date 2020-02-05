e7575f10028507af159479f974118017f304bee9
LNPacio <lucapacilli44@gmail.com>
2020-02-05 16:14:57 +0100

    test button13

--- public/javascripts/funzioni_pagina.js
+++ public/javascripts/funzioni_pagina.js
@@ -127,7 +127,7 @@ function clearTable(){
 $(document).ready(function(){
 	$("#aggiungi_città").click(function(){
 		alert("premuto bottone");
-		$.post("/test");
+		$.postJSON("/test");
 		//$.ajax({type: "POST", url: "test"});
 	});
 });
