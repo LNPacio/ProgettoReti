912128b052b2e117780bb7246db12b0b2302b2ca
marcello <marcello@marcello-pc.homenet.telecomitalia.it>
2020-02-06 23:29:26 +0100

    mappa here

--- public/javascripts/funzioni_pagina.js
+++ public/javascripts/funzioni_pagina.js
@@ -223,6 +223,13 @@ var map = new H.Map(document.getElementById('map'),
   zoom: 12,
   pixelRatio: window.devicePixelRatio || 1
 });
+
+var map2 = new H.Map(document.getElementById('map'),
+  defaultLayers.vector.normal.map,{
+  center: {lat:41.9109, lng:12.4818},
+  zoom: 2,
+  pixelRatio: window.devicePixelRatio || 1
+});
 // add a resize listener to make sure that the map occupies the whole container
 //window.addEventListener('resize', () => map.getViewPort().resize());
 
@@ -277,43 +284,3 @@ function individua(){
   geocode(platform);
 }
 
-function init_map(){
-  function moveMapToBerlin(map){
-    map.setCenter({lat:41.9109, lng:12.4818});
-    map.setZoom(5);
-  }
-  
-  /**
-   * Boilerplate map initialization code starts below:
-   */
-  
-  //Step 1: initialize communication with the platform
-  // In your own code, replace variable window.apikey with your own apikey
-  var platform = new H.service.Platform({
-    apikey: window.apikey
-  });
-  var defaultLayers = platform.createDefaultLayers();
-  
-  //Step 2: initialize a map - this map is centered over Europe
-  var map = new H.Map(document.getElementById('map'),
-    defaultLayers.vector.normal.map,{
-    center: {lat:50, lng:5},
-    zoom: 4,
-    pixelRatio: window.devicePixelRatio || 1
-  });
-  // add a resize listener to make sure that the map occupies the whole container
-  window.addEventListener('resize', () => map.getViewPort().resize());
-  
-  //Step 3: make the map interactive
-  // MapEvents enables the event system
-  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-  
-  // Create the default UI components
-  var ui = H.ui.UI.createDefault(map, defaultLayers);
-  
-  // Now use the map as required...
-  window.onload = function () {
-    moveMapToBerlin(map);
-  }
-}
