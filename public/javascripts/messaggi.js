2890ba5dd85eb77c75226703357ef113b491cae8
LNPacio <lucapacilli44@gmail.com>
2020-02-14 17:04:32 +0100

    getAmici

--- public/javascripts/messaggi.js
+++ public/javascripts/messaggi.js
@@ -108,7 +108,7 @@ function loadRequest(){
 			if (msg.length > 0){
 			$('#listadellerichieste').empty();
 			for(var line of msg)
-				$('#listadellerichieste').append('<tr><td name='+line.id+'>'+line.utente1+'<button class="bottoneaccetta" id='+line.id+'>Accetta</button>'+'</td></tr>');
+				$('#listadellerichieste').append('<tr><td name='+line.id+'>'+line.utente1+'<button class="bottoneaccetta" id='+line.id+' type="button">Accetta</button>'+'</td></tr>');
 			}
 		}
 	});
