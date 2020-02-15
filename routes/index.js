b07c7e95d823bca66c119b9d468e17b27ef01e7a
LNPacio <lucapacilli44@gmail.com>
2020-02-15 20:47:50 +0100

    google oauth

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get('/signupG', function(req, res, next) {
           "name": req.body.name,
           "surname": req.body.surname,
           "email": req.body.email
+	  }
      }));
 });
 
